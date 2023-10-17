b131395f6001528a1c2b660d7a2a95674159ee8c
Antonio <a2llamas@gmail.com>
2023-10-17 09:19:19 +0200

    Ajuste del codigo para abrir paint mediante un boleano

--- ServiciosProcesos/cancelacionProceso/src/CancelacionProceso.java
+++ ServiciosProcesos/cancelacionProceso/src/CancelacionProceso.java
@@ -3,21 +3,32 @@ import java.io.IOException;
 public class CancelacionProceso {
 
     public static void main(String[] args) {
+        boolean notepad = false; //Cambiar a false para abrir Paint
+
+        ProcessBuilder pb;
+        Process process;
 
         try {
-            ProcessBuilder pb = new ProcessBuilder("notepad.exe");
-            Process process = pb.start();
+            if (notepad) {
+                pb = new ProcessBuilder("notepad.exe");
+            } else {
+                pb = new ProcessBuilder("mspaint.exe");
+            }
+
+            process = pb.start();
 
             System.out.println("Proceso iniciado");
             Thread.sleep(5000);
 
             if (process.isAlive()){
                 System.out.println("Proceso vivo");
-
-                ProcessBuilder taskkill = new ProcessBuilder("taskkill", "/F", "/IM", "notepad.exe");
-                Process pro = taskkill.start();
-                pro.waitFor();
-
+                if (notepad){
+                    ProcessBuilder taskkill = new ProcessBuilder("taskkill", "/F", "/IM", "notepad.exe");
+                    Process pro = taskkill.start();
+                    pro.waitFor();
+                } else {
+                    process.destroy();
+                }
             } else {
                 System.out.println("Proceso cerrado manualmente");
             }
