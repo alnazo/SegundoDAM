b7ce36fb7462a2a1628cf85ac9ab83a318e82b5a
Antonio <a2llamas@gmail.com>
2023-10-17 13:58:56 +0200

    Primera app Angular

---
+++ LibreConfiguracion-ProgramacionWeb/primerProyecto/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /* Calculadora
+  title = 'Calculadora de suma';
+
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+
+
+  onSuma():void {
+    this.resultado = this.operandoA + this.operandoB;
+  }
+  */
+
+  title = "Bienvenido a tu aplicación"
+
+  img = "https://http.cat/303.jpg"
+
+}
