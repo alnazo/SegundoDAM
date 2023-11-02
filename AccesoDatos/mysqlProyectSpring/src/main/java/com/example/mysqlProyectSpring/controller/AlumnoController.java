4f6adfe8816c79a73bac07c0cfb0d2fd3d5f4883
Antonio <a2llamas@gmail.com>
2023-11-02 09:02:39 +0100

    Añadido accion de editar y eliminar Alumno

--- AccesoDatos/mysqlProyectSpring/src/main/java/com/example/mysqlProyectSpring/controller/AlumnoController.java
+++ AccesoDatos/mysqlProyectSpring/src/main/java/com/example/mysqlProyectSpring/controller/AlumnoController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,6 +61,11 @@ public class AlumnoController {
         return new RedirectView("/alumnos");
     }
 
-
+    @PostMapping("/eliminaralumno")
+    public RedirectView eliminarAlumno(int id){
+        Optional<Alumno> alumno = alumnoRepository.findById(id);
+        alumno.ifPresent(value -> alumnoRepository.delete(value));
+        return new RedirectView("/alumnos");
+    }
 
 }
